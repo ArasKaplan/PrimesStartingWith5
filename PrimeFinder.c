89af41e60dc400e3e9fd07f9a9a158d6f2523d74
Ömer Aras Kaplan <omeraraskaplan@gmail.com>
2021-04-16 21:07:28 +0300

    Add files via upload

---
+++ PrimeFinder.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main(){
+	
+	int array[900];
+	int nums=0;
+	int i,j;
+	int broken=0;
+	for(i=501;i<=599;i+=2){
+		for(j=3;j<=sqrt(i);j+=2){
+			if(i%j==0){
+				broken=1;
+				break;
+			}
+		}
+		if(broken==1){
+			broken=0;
+			continue;
+		}
+		else{
+			array[nums++]=i;
+		}
+	}
+	for(i=0;i<nums;i++)
+	printf("%d\n",array[i]);
+}
